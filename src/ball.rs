5c10de948961787d1d1ef6ced0398fe61c38942c
kdb04 <nikhil.srivatsa1@gmail.com>
2024-06-12 22:18:02 +0530

    ball

---
+++ src/ball.rs
@@ -0,0 +1,32 @@
+use raylib::core::*;
+
+pub struct ball{
+    pub x: i32,
+    pub y: i32,//positions
+    pub vx: f32,
+    pub vy: f32,//velocities
+}
+
+impl ball{
+    pub fn init(x:i32, y:i32, vx:f32, vy:f32)->Self{
+        ball{x,y,vx,vy}
+    }
+
+    pub fn change_dir(&mut self, screen_width: i32, screen_height: i32){
+        self.x += self.vx as i32;
+        self.y += self.vy as i32;
+
+        if (self.x <= 0 || self.x + 10 >= screen_width){
+            self.vx = -self.vx; //change direction after collision
+        }
+
+        if (self.y <=0 || self.y + 10 >= screen_height){
+            self.vy = -self.vy;
+        }
+
+    }
+
+    pub fn draw(&mut self, d: &mut raylib::prelude::RaylibDrawHandle){
+        d.draw_circle(self.x, self.y, 10, Color::BLUE);
+    }
+}
