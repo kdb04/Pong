3ed7a526df57536454d0e85f585ac14307229cbf
kdb04 <nikhil.srivatsa1@gmail.com>
2024-06-24 15:28:14 +0530

    fixed ig

--- src/main.rs
+++ src/main.rs
@@ -16,17 +16,19 @@ fn main(){
         .title("Ping-Pong")
         .build();
     rl.set_target_fps(60);
+
     let mut ball = Ball::init(SCREEN_WIDTH/2, SCREEN_HEIGHT, 5.0, 5.0);
     let mut paddle1 = Paddle::init(50, (SCREEN_HEIGHT/2)-50, 5.0); //Relative positions and velocity
     let mut paddle2 = Paddle::init(SCREEN_WIDTH-50-10, (SCREEN_HEIGHT/2)-50, 5.0);
 
     while !rl.window_should_close(){
+        let rl_ref = &rl;
+        ball.change_dir(SCREEN_WIDTH, SCREEN_HEIGHT);
+        paddle1.update(SCREEN_HEIGHT, rl_ref.is_key_down(KeyboardKey::KEY_W), rl_ref.is_key_down(KeyboardKey::KEY_S));
+        paddle2.update(SCREEN_HEIGHT, rl_ref.is_key_down(KeyboardKey::KEY_UP), rl_ref.is_key_down(KeyboardKey::KEY_DOWN));
+
         let mut draw = rl.begin_drawing(&thread);
         draw.clear_background(Color::BLACK);
-
-        ball.change_dir(SCREEN_WIDTH, SCREEN_HEIGHT);
-        paddle1.update(SCREEN_HEIGHT, rl.is_key_down(KeyboardKey::KEY_W), rl.is_key_down(KeyboardKey::KEY_S));
-        paddle2.update(SCREEN_HEIGHT, rl.is_key_down(KeyboardKey::KEY_UP), rl.is_key_down(KeyboardKey::KEY_DOWN));
 
         //Collisions
         if (ball.x<=paddle1.x+10) && (ball.y>=paddle1.y) && (ball.y<=paddle1.y+100){
