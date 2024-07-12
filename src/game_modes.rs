6c86fa9f895c3d72af713b5b1d04259ad6a4c212
kdb04 <nikhil.srivatsa1@gmail.com>
2024-07-12 14:22:19 +0530

    somewhat fixed

--- src/game_modes.rs
+++ src/game_modes.rs
@@ -1,76 +1,78 @@
 use raylib::prelude::*;
 use raylib::consts::KeyboardKey;
-
+use raylib::color::Color;
 use crate::ball::Ball;
 use crate::paddle::Paddle;
 
 const SCREEN_WIDTH: i32 = 1000;
 const SCREEN_HEIGHT: i32 = 800;
 
-pub fn single_player(rl: &mut RaylibHandle, ball: &mut Ball, paddle1: &mut Paddle, paddle2: &mut Paddle, score1: &mut i32, score2: &mut i32, timer: &mut f32, thread: &RaylibThread){
-    while !rl.window_should_close(){
-        ball.change_dir(SCREEN_WIDTH, SCREEN_HEIGHT);
-        paddle2.update(SCREEN_HEIGHT, rl.is_key_down(KeyboardKey::KEY_UP), rl.is_key_down(KeyboardKey::KEY_DOWN));
-        //auto-controlling paddle1
-        if ball.y<paddle1.y{
-            paddle1.speed = -5.0;
-        }
-        else if ball.y>(paddle1.y+100){
-            paddle1.speed = 5.0;
-        }
-        else{
-            paddle1.speed = 0.0;
-        }
-        paddle1.update(SCREEN_HEIGHT, false, false);
+pub fn single_player(rl: &mut RaylibHandle, thread: &RaylibThread, ball: &mut Ball, paddle1: &mut Paddle, paddle2: &mut Paddle, score1: &mut i32, score2: &mut i32, timer: &mut f32){
+    let mut draw = rl.begin_drawing(thread);
+    draw.clear_background(Color::BLACK);
 
-        *timer -= rl.get_frame_time();
-        if *timer<=0.0{
-            break;
-        }
+    ball.change_dir(SCREEN_WIDTH, SCREEN_HEIGHT);
+    paddle2.update(SCREEN_HEIGHT, draw.is_key_down(KeyboardKey::KEY_UP), draw.is_key_down(KeyboardKey::KEY_DOWN));
 
-        if (ball.x <= paddle1.x + 10) && (ball.y >= paddle1.y) && (ball.y <= paddle1.y + 100) {
-            ball.vx = -ball.vx;
-        }
-        if (ball.x + 10 >= paddle2.x) && (ball.y >= paddle2.y) && (ball.y <= paddle2.y + 100) {
-            ball.vx = -ball.vx;
-        }
+    //auto-controlling paddle1
+    if ball.y < paddle1.y + 50{
+        paddle1.speed = -5.0;
+    }
+    else if ball.y > paddle1.y + 50{
+        paddle1.speed = 5.0;
+    }
+    else{
+        paddle1.speed = 0.0;
+    }
+    paddle1.update(SCREEN_HEIGHT, false, false);
 
-        if (ball.x+10) >= SCREEN_WIDTH{
-            *score1 += 1;
-            *ball = Ball::init(SCREEN_WIDTH/2, SCREEN_HEIGHT/2, -5.0, -5.0);
-        }
-        if ball.x<=0{
-            *score2 += 1;
-            *ball = Ball::init(SCREEN_WIDTH/2, SCREEN_HEIGHT/2, 5.0, 5.0);
-        }
+    update_game_state(ball, paddle1, paddle2, score1, score2);
+
+    *timer -= draw.get_frame_time();
+
+    draw_game(&mut draw, ball, paddle1, paddle2, *score1, *score2, *timer);
+}
+
+pub fn multi_player(rl: &mut RaylibHandle, thread: &RaylibThread, ball: &mut Ball, paddle1: &mut Paddle, paddle2: &mut Paddle, score1: &mut i32, score2: &mut i32, timer: &mut f32){
+    let mut draw = rl.begin_drawing(thread);
+    draw.clear_background(Color::BLACK);
+
+    ball.change_dir(SCREEN_WIDTH, SCREEN_HEIGHT);
+    paddle1.update(SCREEN_HEIGHT, draw.is_key_down(KeyboardKey::KEY_W), draw.is_key_down(KeyboardKey::KEY_S));
+    paddle2.update(SCREEN_HEIGHT, draw.is_key_down(KeyboardKey::KEY_UP), draw.is_key_down(KeyboardKey::KEY_DOWN));
+
+    update_game_state(ball, paddle1, paddle2, score1, score2);
+
+    *timer -= draw.get_frame_time();
+
+    draw_game(&mut draw, ball, paddle1, paddle2, *score1, *score2, *timer);
+}
+
+fn update_game_state(ball: &mut Ball, paddle1: &Paddle, paddle2: &Paddle, score1: &mut i32, score2: &mut i32){
+    //Collision with left paddle
+    if (ball.x <= paddle1.x + 10) && (ball.y >= paddle1.y) && (ball.y <= paddle2.y + 100){
+        ball.vx = -ball.vx;
+    }
+    if (ball.x + 10 >= paddle2.x) && (ball.y >= paddle2.y) && (ball.y <= paddle2.y + 100){
+        ball.vx = -ball.vx;
+    }
+    if (ball.x + 10) >= SCREEN_WIDTH{
+        *score1 += 1;
+        *ball = Ball::init(SCREEN_WIDTH/2, SCREEN_HEIGHT/2, -5.0, -5.0);
+    }
+    if ball.x <= 0{
+        *score2 += 1;
+        *ball = Ball::init(SCREEN_WIDTH/2, SCREEN_HEIGHT/2, 5.0, 5.0);
     }
 }
 
-pub fn multi_player(rl: &mut RaylibHandle, ball: &mut Ball, paddle1: &mut Paddle, paddle2: &mut Paddle, score1: &mut i32, score2: &mut i32, timer: &mut f32, thread: &RaylibThread){
-    while !rl.window_should_close(){
-        ball.change_dir(SCREEN_WIDTH, SCREEN_HEIGHT);
-        paddle1.update(SCREEN_HEIGHT, rl.is_key_down(KeyboardKey::KEY_W), rl.is_key_down(KeyboardKey::KEY_S));
-        paddle2.update(SCREEN_HEIGHT, rl.is_key_down(KeyboardKey::KEY_UP), rl.is_key_down(KeyboardKey::KEY_DOWN));
+fn draw_game(draw: &mut RaylibDrawHandle, ball: &mut Ball, paddle1: &Paddle, paddle2: &Paddle, score1: i32, score2: i32, timer: f32){
+    draw.draw_text(&format!("Player 1: {}", score1), 10, 10, 30, Color::WHITE);
+    draw.draw_text(&format!("Player 2: {}", score2), SCREEN_WIDTH - 150, 10, 30, Color::WHITE);
+    draw.draw_text(&format!("Timer: {}", timer), SCREEN_WIDTH/2 - 50, 10, 30, Color::WHITE);
+    draw.draw_text("Press 1 for Multiplayer, 2 for Single Player", 10, SCREEN_HEIGHT - 30, 20, Color::WHITE);
 
-        *timer -= rl.get_frame_time();
-        if *timer<=0.0{
-            break;
-        }
-
-        if (ball.x <= paddle1.x + 10) && (ball.y >= paddle1.y) && (ball.y <= paddle1.y + 100) {
-            ball.vx = -ball.vx;
-        }
-        if (ball.x + 10 >= paddle2.x) && (ball.y >= paddle2.y) && (ball.y <= paddle2.y + 100) {
-            ball.vx = -ball.vx;
-        }
-
-        if (ball.x+10)>=SCREEN_WIDTH{
-            *score1 += 1;
-            *ball = Ball::init(SCREEN_WIDTH/2, SCREEN_HEIGHT/2, -5.0, -5.0);
-        }
-        if ball.x<=0{
-            *score2 += 1;
-            *ball = Ball::init(SCREEN_WIDTH/2, SCREEN_HEIGHT/2, 5.0, 5.0);
-        }
-    }
+    paddle1.draw(draw);
+    paddle2.draw(draw);
+    ball.draw(draw);
 }
--- src/main.rs
+++ src/main.rs
@@ -29,66 +29,31 @@ fn main(){
     let mut timer = 60.0; //game terminates after 60 seconds
 
     let mut game_mode = 1; //default: multi-player
+    
+    while !rl.window_should_close() {
+        match rl.get_key_pressed(){
+            Some(KeyboardKey::KEY_ONE) => {
+                game_mode = 1;
+                reset_game(&mut ball, &mut paddle1, &mut paddle2, &mut score1, &mut score2, &mut timer);
+            },
+            Some(KeyboardKey::KEY_TWO) => {
+                game_mode = 2;
+                reset_game(&mut ball, &mut paddle1, &mut paddle2, &mut score1, &mut score2, &mut timer);
+            },
+            _ => {}
+        }
 
-    match game_mode{
-        1 =>{
-            while !rl.window_should_close(){
-                match rl.get_key_pressed(){
-                    Some(KeyboardKey::KEY_ONE)=>{
-                        game_mode = 1;
-                    },
-                    Some(KeyboardKey::KEY_TWO)=>{
-                        game_mode = 2;
-                    },
-                    _ => {}
-                }
-                ball.change_dir(SCREEN_WIDTH, SCREEN_HEIGHT);
-                paddle1.update(SCREEN_HEIGHT, rl.is_key_down(KeyboardKey::KEY_W), rl.is_key_down(KeyboardKey::KEY_S));
-                paddle2.update(SCREEN_HEIGHT, rl.is_key_down(KeyboardKey::KEY_UP), rl.is_key_down(KeyboardKey::KEY_DOWN));
-
-                //Countdown timer
-                timer -= rl.get_frame_time();
-                if timer<=0.0{
-                    break;
-                }
-
-                let mut draw = rl.begin_drawing(&thread);
-                draw.clear_background(Color::BLACK);
-
-                //Collisions
-                if (ball.x<=paddle1.x+10) && (ball.y>=paddle1.y) && (ball.y<=paddle1.y+100){
-                    ball.vx = -ball.vx;
-                }
-                if (ball.x+10>=paddle2.x) && (ball.y>=paddle2.y) && (ball.y<=paddle2.y+100){
-                    ball.vx = -ball.vx;
-                }
-
-                //Scoreboard
-                if (ball.x + 10)>=SCREEN_WIDTH{
-                    score1+=1;
-                    ball = Ball::init(SCREEN_WIDTH/2, SCREEN_HEIGHT/2, -5.0, -5.0);
-                }
-                if ball.x<=0{
-                    score2+=1;
-                    ball = Ball::init(SCREEN_WIDTH/2, SCREEN_HEIGHT/2, 5.0, 5.0);
-                }
-
-                //Display scores
-                draw.draw_text(format!("Player1:{}", score1).as_str(), 10, 10, 30, Color::WHITE);
-                draw.draw_text(format!("Player2:{}", score2).as_str(), SCREEN_WIDTH as i32 - 150, 10, 30, Color::WHITE);
-                draw.draw_text(format!("Timer:{:.0}",timer).as_str(), SCREEN_WIDTH as i32 /2 - 50, 10, 30, Color::WHITE);
-
-                paddle1.draw(&mut draw);
-                paddle2.draw(&mut draw);
-                ball.draw(&mut draw);
+        match game_mode {
+            1 => multi_player(&mut rl,  &thread, &mut ball, &mut paddle1, &mut paddle2, &mut score1, &mut score2, &mut timer),
+            2 => single_player(&mut rl, &thread, &mut ball, &mut paddle1, &mut paddle2, &mut score1, &mut score2, &mut timer),
+            _ => {
+                println!("Invalid game mode!");
+                return;
             }
-        },
-        2 => {
-            single_player(&mut rl, &mut ball, &mut paddle1, &mut paddle2, &mut score1, &mut score2, &mut timer, &thread)
-        },
-        _ => {
-            println!("Invalid game mode!");
-            return;
+        }
+        
+        if timer <= 0.0{
+            break;
         }
     }
 
@@ -104,4 +69,19 @@ fn main(){
     else{
         draw.draw_text("Tie!", SCREEN_WIDTH as i32 /2 - 100, SCREEN_HEIGHT as i32 /2, 50, Color::WHITE);
     }
+    draw.draw_text("Press ESC to exit", SCREEN_WIDTH/2 - 100, SCREEN_HEIGHT/2 + 60, 30, Color::WHITE);
+    drop(draw);
+
+    while !rl.window_should_close() {
+        rl.begin_drawing(&thread);
+    }
+}
+
+fn reset_game(ball: &mut Ball, paddle1: &mut Paddle, paddle2: &mut Paddle, score1: &mut i32, score2: &mut i32, timer: &mut f32){
+    *ball = Ball::init(SCREEN_WIDTH/2, SCREEN_HEIGHT/2, 5.0, 5.0);
+    *paddle1 = Paddle::init(50, (SCREEN_HEIGHT/2) - 50, 5.0);
+    *paddle2 = Paddle::init(SCREEN_WIDTH - 50 - 10, (SCREEN_HEIGHT/2) - 50, 5.0);
+    *score1 = 0;
+    *score2 = 0;
+    *timer = 60.0;
 }
